626b0e3502c16b7e1e7272ac9569a280bbdcaa35
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-11-11 14:17:21 -0300

    Encriptando as senhas dos usuários

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder.matches("123456", "$2a$10$UdDwRu8nlVHINQERZCqmCOGdgkwIDR4o1kTm8QKE1zDJAvI2xiHf6");
+		System.out.println("RESULTADO = " + result);
+		
 	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$UdDwRu8nlVHINQERZCqmCOGdgkwIDR4o1kTm8QKE1zDJAvI2xiHf6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$UdDwRu8nlVHINQERZCqmCOGdgkwIDR4o1kTm8QKE1zDJAvI2xiHf6');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
